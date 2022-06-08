76b71b66adaff8816839e60d2d29c13fd39b4ce4
Santhosh-user <85502994+Santhosh-user@users.noreply.github.com>
2022-06-08 21:39:21 +0530

    1

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,23 +1,42 @@
 import React, { useState } from "react";
 import styles from "./addTask.module.css";
+import axios from "axios"
 
 const AddTask = () => {
   // NOTE: do not delete `data-testid` key value pair
 
   const[inpTodo, setinpTodo] = useState("")
 
+  const[saveTodo, setsaveTodo] = useState("")
+
   const proTodo=()=>{
-    if(inpTodo.length<=0){
-      alert("input is empty")
+    if(saveTodo.length<=0){
+      alert("empty")
       return
-      
     }
+
+    axios.post('http://localhost:8080/tasks', {
+      id: "",
+      text: saveTodo,
+      done: false,
+      count: 0,
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
   }
 
  const exTodo=(e)=>{
     const {id,value} = e.target
-    setinpTodo(value)
+    setsaveTodo(value)
  }
+
+
+ 
 
 
   return (
@@ -25,6 +44,7 @@ const AddTask = () => {
       <input data-testid="add-task-input" type="text" onChange={exTodo}/>
       <button data-testid="add-task-button"  onClick={proTodo}>+</button>
     </div>
+    
   );
 };
 
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./task.module.css";
 import Counter from "../Counter/Counter"
+import axios from "axios";
 
 const Task = () => {
   // NOTE: do not delete `data-testid` key value pair
+
+  const [geTodo, setgeTodo] = useState([])
+
+  useEffect(()=>{
+    getTodo()
+  },[])
+
+  const getTodo=()=>{
+    axios.get('http://localhost:8080/tasks')
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  }); 
+  } 
+
+
   return (
     <li data-testid="task" className={styles.task}>
       <input type="checkbox" data-testid="task-checkbox" />
